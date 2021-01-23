8ca5d91007d3607597d65407f25b70eb5b753a18
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2021-01-23 17:41:30 +0700

    Update usernew.sh

--- debian9/usernew.sh
+++ debian9/usernew.sh
@@ -5,7 +5,7 @@ read -p "Username : " Login
 read -p "Password : " Pass
 read -p "Expired (hari): " masaaktif
 
-IP=`dig +short myip.opendns.com @resolver1.opendns.com`
+IP=$(wget -qO- ipv4.icanhazip.com)
 useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
@@ -20,6 +20,7 @@ echo -e "SSL/TLS SSH : 443" | lolcat
 echo -e "SSL/TLS OpenSSH : 43,600,700,800,900" | lolcat
 echo -e "SSL/TLS Dropbear : 444,540,551,777,9900" | lolcat
 echo -e "SSL/TLS SSR SSH : 69" | lolcat
+echo -e "SSL/TLS OpenVPN : 569" | lolcat
 echo -e "BadVPN-UDPGW : 7100,7200,7300" | lolcat
 echo -e "Speed Server : 2 Gbps" | lolcat
 echo -e "Transfer : 2 TB" | lolcat
