f04a2fdf10552844e8385cb504d91c5136fcb38e
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2021-01-25 17:04:43 +0700

    Update kamu.sh

--- debian9/kamu.sh
+++ debian9/kamu.sh
@@ -46,41 +46,35 @@ do
                 echo "$PID - $USER - $IP";
         fi
 done
-if [ -f "/etc/openvpn/server-vpn.log" ]; then
-	line=`cat /etc/openvpn/server-vpn.log | wc -l`
+echo " "
+echo "Memerika User OpenVPN Yang Login 1";
+echo "(ID - Username - IP)";
+echo "-------------------------------------";
+cat $LOG | grep -i openvpn | grep -i "Accepted password for" > /tmp/login-db.txt
+data=( `ps aux | grep openvpn | awk '{print $2}'`);
+
+for PID in "${data[@]}"
+do
+        cat /tmp/login-db.txt | grep "openvpn\[$PID\]" > /tmp/login-db-pid.txt;
+        NUM=`cat /var/log/syslog | wc -l`;
+        USER=`cat /var/log/syslog | awk '{print $9}'`;
+        IP=`cat /var/log/syslog | awk '{print $11}'`;
+        if [ $NUM -eq 1 ]; then
+                echo "$PID - $USER - $IP";
+        fi
+done
+if [ -f "/var/log/syslog" ]; then
+	line=`cat /var/log/syslog | wc -l`
 	a=$((3+((line-8)/2)))
 	b=$(((line-8)/2))
 	echo " "
-	echo "Memerika User OpenVPN Yang Login";
+	echo "Memerika User OpenVPN Yang Login 2";
 	echo "(Username - IP - Terkoneksi Sejak)";
 	echo "-------------------------------------";
-	cat /etc/openvpn/server-vpn.log | head -n $a | tail -n $b | cut -d "," -f 1,2,5 | sed -e 's/,/   /g' > /tmp/vpn-login-db.txt
+	cat /var/log/syslog | head -n $a | tail -n $b | cut -d "," -f 1,2,5 | sed -e 's/,/   /g' > /tmp/vpn-login-db.txt
 	cat /tmp/vpn-login-db.txt
 fi
 done
-if [ -f "/etc/openvpn/server-tcp.log" ]; then
-	line=`cat /etc/openvpn/server-tcp.log | wc -l`
-	a=$((3+((line-8)/2)))
-	b=$(((line-8)/2))
-	echo " "
-	echo "Memerika User OpenVPN Yang Login";
-	echo "(Username - IP - Terkoneksi Sejak)";
-	echo "-------------------------------------";
-	cat /etc/openvpn/server-tcp.log | head -n $a | tail -n $b | cut -d "," -f 1,2,5 | sed -e 's/,/   /g' > /tmp/vpn-login-db.txt
-	cat /tmp/vpn-login-db.txt
-fi
-done
-if [ -f "/etc/openvpn/server-udp.log" ]; then
-	line=`cat /etc/openvpn/server-udp.log | wc -l`
-	a=$((3+((line-8)/2)))
-	b=$(((line-8)/2))
-	echo " "
-	echo "Memerika User OpenVPN Yang Login";
-	echo "(Username - IP - Terkoneksi Sejak)";
-	echo "-------------------------------------";
-	cat /etc/openvpn/server-udp.log | head -n $a | tail -n $b | cut -d "," -f 1,2,5 | sed -e 's/,/   /g' > /tmp/vpn-login-db.txt
-	cat /tmp/vpn-login-db.txt
-fi
 
 echo " "
 echo " "
