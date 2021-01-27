a22b989ac97c1e42073ef84432f27d96233c8b1c
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2021-01-27 08:23:54 +0700

    Update openssh.sh

--- debian9/openssh.sh
+++ debian9/openssh.sh
@@ -405,7 +405,7 @@ wget -O speedtest "https://raw.githubusercontent.com/fisabiliyusri/sshsl/master/
 wget -O info "https://raw.githubusercontent.com/fisabiliyusri/sshsl/master/debian9/info.sh"
 wget -O shadowsl "https://raw.githubusercontent.com/fisabiliyusri/Betatest/master/debian9/Auto-Install-Shadowsocks-SL.sh"
 wget -O monitorvpn "https://raw.githubusercontent.com/fisabiliyusri/Betatest/master/auto-install-openvpn-monitor.sh"
-wget --no-check-certificate -O shadowsocks-all https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh"
+wget --no-check-certificate -O shadowsocks-all "https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh"
 wget -O about "https://raw.githubusercontent.com/fisabiliyusri/sshsl/master/debian9/about.sh"
 wget -O delete "https://raw.githubusercontent.com/fisabiliyusri/sshsl/master/debian9/delete.sh"
 
