d63e342d30393aa8dedb18b8c26c2748182b9e2b
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2021-01-27 08:53:55 +0700

    Update auto-install-openvpn-monitor.sh

--- auto-install-openvpn-monitor.sh
+++ auto-install-openvpn-monitor.sh
@@ -9,20 +9,20 @@ sed -i 's/80/99/g' /etc/apache2/ports.conf
 /etc/init.d/apache2 restart
 
 # Install dependencies and configure apache
-```shell
+
 apt-get -y install python-geoip python-ipaddr python-humanize python-bottle python-semantic-version apache2 libapache2-mod-wsgi git wget geoip-database-extra
 echo "WSGIScriptAlias /openvpn-monitor /var/www/html/openvpn-monitor/openvpn-monitor.py" > /etc/apache2/conf-available/openvpn-monitor.conf
 a2enconf openvpn-monitor
 systemctl restart apache2
 a2enconf openvpn-monitor
 /etc/init.d/apache2 restart
-```
+
 
 # Checkout OpenVPN-Monitor
-```shell
+
 cd /var/www/html
 git clone https://github.com/fisabiliyusri/openvpn-monitor.git
-```
+
 
 #
 ```shell
