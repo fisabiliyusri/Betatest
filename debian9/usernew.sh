30f5bb63961bc0e313ee62416b212157c20567dd
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2021-02-01 15:04:16 +0700

    Update usernew.sh

--- debian9/usernew.sh
+++ debian9/usernew.sh
@@ -1,44 +1,4 @@
 # !/bin/bash
-# openvpn config
-# initialisasi var
-export DEBIAN_FRONTEND=noninteractive
-OS=`uname -m`;
-MYIP=$(wget -qO- ipv4.icanhazip.com);
-MYIP2="s/xxxxxxxxx/$MYIP/g";
-# hapus
-#rm -rf /etc/openvpn/client-udp.ovpn
-#rm -rf /etc/openvpn/client-tcp.ovpn
-#rm -rf /etc/openvpn/569client-ssl.ovpn
-#rm -rf /home/vps/public_html/client-udp.ovpn
-#rm -rf /home/vps/public_html/client-tcp.ovpn
-#rm -rf /home/vps/public_html/569client-ssl.ovpn
-
-# buat udp
-cd /etc/openvpn/
-wget -O /etc/openvpn/client-udp.ovpn "https://raw.githubusercontent.com/fisabiliyusri/Betatest/master/debian9/client-udp.conf"
-sed -i $MYIP2 /etc/openvpn/client-udp.ovpn;
-echo '<ca>' >> /etc/openvpn/client-udp.ovpn
-cat /etc/openvpn/ca.crt >> /etc/openvpn/client-udp.ovpn
-echo '</ca>' >> /etc/openvpn/client-udp.ovpn
-cp /etc/openvpn/client-udp.ovpn /home/vps/public_html
-
-#buat tcp
-wget -O /etc/openvpn/client-tcp.ovpn "https://raw.githubusercontent.com/fisabiliyusri/Betatest/master/debian9/client-tcp.conf"
-sed -i $MYIP2 /etc/openvpn/client-tcp.ovpn;
-echo '<ca>' >> /etc/openvpn/client-tcp.ovpn
-cat /etc/openvpn/ca.crt >> /etc/openvpn/client-tcp.ovpn
-echo '</ca>' >> /etc/openvpn/client-tcp.ovpn
-cp /etc/openvpn/client-tcp.ovpn /home/vps/public_html/
-
-#buat ssl
-wget -O /etc/openvpn/569client-ssl.ovpn "https://raw.githubusercontent.com/fisabiliyusri/Betatest/master/569client-ssl.conf"
-sed -i $MYIP2 /etc/openvpn/569client-ssl.ovpn;
-echo '<ca>' >> /etc/openvpn/569client-ssl.ovpn
-cat /etc/openvpn/ca.crt >> /etc/openvpn/569client-ssl.ovpn
-echo '</ca>' >> /etc/openvpn/569client-ssl.ovpn
-cp /etc/openvpn/569client-ssl.ovpn /home/vps/public_html/
-
-
 # Script auto create user SSH
 
 read -p "Username : " Login
@@ -71,13 +31,17 @@ echo -e "Aktif Sampai      : $exp"
 echo -e "============================="
 echo -e "Mod by Sulaiman L" 
 echo -e "=============================" 
+echo -e "OpenVPN TCP: 56969" 
+echo -e "OpenVPN UDP : 1945" 
+echo -e "OpenVPN TLS/SSL : 569" 
 echo -e "Config OpenVPN (TCP 56969): "
-echo -e "http:// $IP :81/client-tcp.ovpn" 
+echo -e "*$IP :81/client-tcp.ovpn" 
 echo -e "=============================" 
 echo -e "Config OpenVPN (UDP 1945): "
-echo -e "http:// $IP :81/client-udp.ovpn"
+echo -e "*$IP :81/client-udp.ovpn"
 echo -e "=============================" 
 echo -e "Config OpenVPN (TCP+SSL 569): "
-echo -e "http:// $IP :81/569client-ssl.ovpn"
+echo -e "*$IP :81/569client-ssl.ovpn"
+echo -e "hapus tanda *"
 echo -e "=============================" 
 echo -e ""
