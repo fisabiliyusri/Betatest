01e656ba6acc54b6ff1343b6a8437d67b1b8e8b2
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2021-02-04 19:30:04 +0700

    Create install-config.sh

---
+++ install-config.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Kuota Maxstream Tsel
+wget -O /home/vps/public_html/SL-Maxstream.ehi "https://raw.githubusercontent.com/fisabiliyusri/Betatest/master/SL-Maxstream.ehi"
+
+# Kuota Whatsapp/Kemdikbud Tsel
+wget -O /home/vps/public_html/SL-Wa-Kemdikbud.ehi "https://raw.githubusercontent.com/fisabiliyusri/Betatest/master/SL-Wa-Kemdikbud.ehi"
+
+# Kuota Youtube Tsel
+wget -O /home/vps/public_html/SL-YoutubeTsel.ehi "https://raw.githubusercontent.com/fisabiliyusri/Betatest/master/SL-YoutubeTsel.ehi"
+
+echo -e "Maxstream Tsel" 
+echo -e "/home/vps/public_html/SL-Maxstream.ehi"
+echo -e "Whatsapp/Kemdikbud Tsel" 
+echo -e "/home/vps/public_html/SL-Wa-Kemdikbud.ehi"
+echo -e "Youtube Tsel" 
+echo -e "/home/vps/public_html/SL-YoutubeTsel.ehi"
+echo -e "Install Config Berhasil" 
+echo -e "Mod by Sulaiman L" 
