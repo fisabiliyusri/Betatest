8c6020d764781a0dee68739a2e51f31851309f55
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2021-02-04 20:08:00 +0700

    Update cekconfig.sh

--- cekconfig.sh
+++ cekconfig.sh
@@ -1,27 +1,27 @@
 # !/bin/bash
 # cekconfig = melihat daftar config
 
-echo -e "List Config Yang Tersedia" | lolcat
+echo -e "  List Config Yang Tersedia" | lolcat
 echo -e "============================="| lolcat
-echo -e "Config HI Maxstream (Tsel)" | lolcat
+echo -e "  Config HI Maxstream (Tsel)" | lolcat
 echo -e "Host: 81/SL-Maxstream.ehi" | lolcat
 echo -e "=============================" | lolcat
-echo -e "Config HI Whatsapp/Kemdibud (Tsel)" | lolcat
+echo -e "  Config HI Whatsapp/Kemdibud (Tsel)" | lolcat
 echo -e "Host: 81/SL-Wa-Kemdikbud.ehi" | lolcat
 echo -e "=============================" | lolcat
-echo -e "Config HI Youtube (Tsel)" | lolcat
+echo -e "  Config HI Youtube (Tsel)" | lolcat
 echo -e "Host: 81/SL-YoutubeTsel.ehi" | lolcat
 echo -e "=============================" | lolcat
-echo -e "Config OpenVPN TCP 56969:" | lolcat
+echo -e "  Config OpenVPN TCP 56969:" | lolcat
 echo -e "Host: 81/client-tcp.ovpn" | lolcat
 echo -e "=============================" | lolcat
-echo -e "Config OpenVPN UDP 1945: " | lolcat
+echo -e "  Config OpenVPN UDP 1945: " | lolcat
 echo -e "Host: 81/client-udp.ovpn " | lolcat
 echo -e "=============================" | lolcat
-echo -e "Config OpenVPN SSL 569:" | lolcat
+echo -e "  Config OpenVPN SSL 569:" | lolcat
 echo -e "Host: 81/569client-ssl.ovpn" | lolcat
 echo -e "=============================" | lolcat
-echo -e "Config ehi (HTTP Injector) " | lolcat
-echo -e "Config ovpn " | lolcat
+echo -e "  Config ehi (HTTP Injector) " | lolcat
+echo -e "  Config ovpn " | lolcat
 echo -e "(OpenVPN HTTP Custom(Open Cfg VPN))" | lolcat
-echo -e "Mod by Sulaiman L"  | lolcat
+echo -e "  Mod by Sulaiman L"  | lolcat
