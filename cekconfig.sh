6be8c7fc264f16754bbfb8969a8563ae592059fe
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2021-02-04 19:59:11 +0700

    Update cekconfig.sh

--- cekconfig.sh
+++ cekconfig.sh
@@ -1,27 +1,27 @@
 # !/bin/bash
 # cekconfig = melihat daftar config
 
-echo -e "List Config Yang Tersedia"
-echo -e "============================="
-echo -e "Config HI Maxstream (Tsel)"
-echo -e "Host: 81/SL-Maxstream.ehi"
-echo -e "============================="
-echo -e "Config HI Whatsapp/Kemdibud (Tsel)"
-echo -e "Host: 81/SL-Wa-Kemdikbud.ehi"
-echo -e "============================="
-echo -e "Config HI Youtube (Tsel)"
-echo -e "Host: 81/SL-YoutubeTsel.ehi"
-echo -e "============================="
-echo -e "Config OpenVPN TCP 56969:"
-echo -e "Host: 81/client-tcp.ovpn"
-echo -e "============================="
-echo -e "Config OpenVPN UDP 1945: "
-echo -e "Host: 81/client-udp.ovpn "
-echo -e "============================="
-echo -e "Config OpenVPN SSL 569:"
-echo -e "Host: 81/569client-ssl.ovpn"
-echo -e "============================="
-echo -e "Config ehi (HTTP Injector) "
-echo -e "Config ovpn "
-echo -e "(OpenVPN HTTP Custom(Open Cfg VPN))"
-echo -e "Mod by Sulaiman L" 
+echo -e "List Config Yang Tersedia" | lolcat
+echo -e "============================="| lolcat
+echo -e "Config HI Maxstream (Tsel)" | lolcat
+echo -e "Host: 81/SL-Maxstream.ehi" | lolcat
+echo -e "=============================" | lolcat
+echo -e "Config HI Whatsapp/Kemdibud (Tsel)" | lolcat
+echo -e "Host: 81/SL-Wa-Kemdikbud.ehi" | lolcat
+echo -e "=============================" | lolcat
+echo -e "Config HI Youtube (Tsel)" | lolcat
+echo -e "Host: 81/SL-YoutubeTsel.ehi" | lolcat
+echo -e "=============================" | lolcat
+echo -e "Config OpenVPN TCP 56969:" | lolcat
+echo -e "Host: 81/client-tcp.ovpn" | lolcat
+echo -e "=============================" | lolcat
+echo -e "Config OpenVPN UDP 1945: " | lolcat
+echo -e "Host: 81/client-udp.ovpn " | lolcat
+echo -e "=============================" | lolcat
+echo -e "Config OpenVPN SSL 569:" | lolcat
+echo -e "Host: 81/569client-ssl.ovpn" | lolcat
+echo -e "=============================" | lolcat
+echo -e "Config ehi (HTTP Injector) " | lolcat
+echo -e "Config ovpn " | lolcat
+echo -e "(OpenVPN HTTP Custom(Open Cfg VPN))" | lolcat
+echo -e "Mod by Sulaiman L"  | lolcat
