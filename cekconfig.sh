a4389e83ff66c13b1f69bbb27708015ff62e411e
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2021-02-04 19:41:59 +0700

    Create cekconfig.sh

---
+++ cekconfig.sh
@@ -0,0 +1,28 @@
+# !/bin/bash
+# cekconfig = melihat daftar config
+
+echo -e "List Config Yang Tersedia"
+echo -e "============================="
+echo -e "Config HI Maxstream (Tsel)"
+echo -e "Host: 81/SL-Maxstream.ehi"
+echo -e "============================="
+Config HI Whatsapp/Kemdibud (Tsel)
+echo -e "Host: 81/SL-Wa-Kemdikbud.ehi"
+echo -e "============================="
+echo -e "Config HI Youtube (Tsel)"
+echo -e "Host: 81/SL-YoutubeTsel.ehi"
+echo -e "============================="
+echo -e "Config OpenVPN TCP 56969:"
+echo -e "Host: 81/client-tcp.ovpn"
+echo -e "============================="
+echo -e "Config OpenVPN UDP 1945: "
+echo -e "Host: 81/client-udp.ovpn "
+echo -e "============================="
+echo -e "Config OpenVPN SSL 569:"
+echo -e "Host: 81/569client-ssl.ovpn"
+echo -e "============================="
+echo -e "Config ehi (HTTP Injector) "
+echo -e "Config ovpn "
+echo -e "(OpenVPN HTTP Custom(Open Cfg VPN))"
+
+Mod By Sulaiman L
