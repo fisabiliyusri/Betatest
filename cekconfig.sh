881396e58e15dfdade52d5270e9b133f298639d5
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2021-02-09 02:06:54 +0700

    Update cekconfig.sh

--- cekconfig.sh
+++ cekconfig.sh
@@ -4,22 +4,22 @@
 echo -e "  List Config Yang Tersedia" | lolcat
 echo -e "============================="| lolcat
 echo -e "  Config HI Maxstream (Tsel)" | lolcat
-echo -e "Host: 81/SL-Maxstream.ehi" | lolcat
+echo -e "Host: 85/SL-Maxstream.ehi" | lolcat
 echo -e "=============================" | lolcat
 echo -e "  Config HI Whatsapp/Kemdibud (Tsel)" | lolcat
-echo -e "Host: 81/SL-Wa-Kemdikbud.ehi" | lolcat
+echo -e "Host: 85/SL-Wa-Kemdikbud.ehi" | lolcat
 echo -e "=============================" | lolcat
 echo -e "  Config HI Youtube (Tsel)" | lolcat
-echo -e "Host: 81/SL-YoutubeTsel.ehi" | lolcat
+echo -e "Host: 85/SL-YoutubeTsel.ehi" | lolcat
 echo -e "=============================" | lolcat
 echo -e "  Config OpenVPN TCP 56969:" | lolcat
-echo -e "Host: 81/client-tcp.ovpn" | lolcat
+echo -e "Host: 85/client-tcp.ovpn" | lolcat
 echo -e "=============================" | lolcat
 echo -e "  Config OpenVPN UDP 1945: " | lolcat
-echo -e "Host: 81/client-udp.ovpn " | lolcat
+echo -e "Host: 85/client-udp.ovpn " | lolcat
 echo -e "=============================" | lolcat
 echo -e "  Config OpenVPN SSL 569:" | lolcat
-echo -e "Host: 81/569client-ssl.ovpn" | lolcat
+echo -e "Host: 85/569client-ssl.ovpn" | lolcat
 echo -e "=============================" | lolcat
 echo -e "  Config ehi (HTTP Injector) " | lolcat
 echo -e "  Config ovpn " | lolcat
